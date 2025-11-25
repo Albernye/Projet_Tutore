0bde06fc3dd3b24e4087e593a00769bc1e2b7b84
Albernye <albernye@gmail.com>
2025-11-25 15:04:42 +0100

    Add initial files for C003 control system project
    Added MATLAB scripts, Simulink models, and CSV data for the (C003) project, including state-space and observer design implementations. This initial commit sets up the project structure and provides all necessary source and data files for further development.

---
+++ C003/Test.m
@@ -0,0 +1,95 @@
+clear;
+close;
+clc;
+
+%% Fonction de transfert
+s = tf('s');
+
+Batt_Voltage = 7.2;
+K1 = 0.01;
+K2 = 0.5036;
+K3 = K2;
+
+TF_inter_1 = 1/(4.7e-3*s+5.11);
+TF_inter_2 = 1/(0.0021*s + 0.0012);
+
+TF = K1*Batt_Voltage*((TF_inter_1*K2*TF_inter_2)/(1+K3*TF_inter_1*K2*TF_inter_2));
+TF2 = TF/s;
+P = pole(TF);
+
+K = dcgain(TF)
+
+TF_zpk = 1/(s-P(3));
+
+K2 = dcgain(TF_zpk);
+new_K = K/K2;
+
+TF_zpk = TF_zpk*new_K;
+TF_U_theta = TF_zpk/s;
+
+dcgain(TF_zpk)
+
+%% Representation d'etat matrice compagne
+A = compan(cell2mat(TF_U_theta.Denominator));
+B = [1 0]';
+tmp = cell2mat(TF_U_theta.Numerator);
+
+
+denom = cell2mat(TF_U_theta.Denominator);
+num = cell2mat(TF_U_theta.Numerator);
+
+A = [0 1;-denom(3) -denom(2)];
+B = [num(2) num(3)]';
+C = [1 0];
+D = 0;
+
+
+%% Echantillonnage
+Te = 1/100;
+Sc = ss(A,B,C,D);
+Sd = c2d(Sc,Te,'zoh');
+
+Ad = Sd.A;
+Bd = Sd.B;
+Cd = Sd.C;
+Dd = Sd.D;
+
+poles_souhaites = [0.9412-0.0741i 0.9412+0.0741i];
+
+K_corr = acker(Ad,Bd,poles_souhaites);
+
+
+S_corr = inv(Cd*inv(eye(2)-Ad+Bd*K_corr)*Bd)
+
+
+%% Terme integrale
+poles_souhaites = [0.9 0.9412-0.0741i 0.9412+0.0741i];
+A_t = [Ad zeros(size(Ad,1),1);Cd ones(size(Cd,1),1)];
+B_t = [Bd;Dd];
+
+K_t = acker(A_t,B_t,poles_souhaites);
+
+K_int = K_t(1:2);
+H_barre = K_t(3);
+
+
+%% Observateur
+n_C = rank(obsv(Ad,Cd)); % n_C = 2 donc observable (p = 2)
+I_p = [eye(2) [0;0]];
+
+
+% Calcul du gain d'observateur L
+poles_observateur = [0.5 0.55]; % Pôles souhaités pour l'observateur
+Lo = acker(Ad', Cd', poles_observateur)'; % Gain d'observateur
+
+Aobs = Ad-Lo*Cd;
+Bobs = [Bd Lo];
+Cobs = eye(2);
+Dobs = zeros(2);
+
+%% Calcul valeur propre
+
+MATRICE = [Ad-Bd*K_int Bd*H_barre;
+       -Cd+Dd*K_int 1-Dd*H_barre];
+   
+eig(MATRICE)
