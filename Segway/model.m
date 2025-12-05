a874036f07d4c69ceaa560ad9eb8a46720074fac
carolinelb1 <caroline.laroumagne-boniol@etu.unistra.fr>
2025-12-05 12:02:52 +0100

    Simulink retour d'etat

--- Segway/model.m
+++ Segway/model.m
@@ -82,11 +82,11 @@ Dd = s2.D;
 
 %% Retour d'état
 
-poles_souhaites = [0.3314 0.9739 0.7 0.9];
+poles_souhaites = [0.6 0.9739 0.98 0.89];
 
 K_corr = acker(Ad,Bd,poles_souhaites);
 
-S_corr = inv([1 0 0 0]*inv(eye(4)-Ad+Bd*K_corr)*Bd);
+S_corr = inv([0 1 0 0]*inv(eye(4)-Ad+Bd*K_corr)*Bd);
 
 %% Terme integrale
 % poles_souhaites = [0.9412-0.0741i 0.9412+0.0741i 0.9];
