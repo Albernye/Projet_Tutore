f6aadd14d52c9bed6ed5fc308fe881512b9db6a9
carolinelb1 <caroline.laroumagne-boniol@etu.unistra.fr>
2026-02-12 17:11:13 +0100

    lecture csv

--- MCC/2020/C003/lecture_csv.m
+++ MCC/2020/C003/lecture_csv.m
@@ -1,3 +1,17 @@
-%clear;
+clear;
 
-T = readcsv("donnees.csv");
+T = readtable("donees.csv");
+
+
+N = length(T.reference);
+t = linspace(0,12,N);
+
+
+figure(1)
+hold on
+plot(t,T.reference)
+plot(t,T.systemeReel)
+plot(t,T.termeIntegral)
+plot(t,T.observateur)
+plot(t,T.observateurEtTermeIntegral)
+legend('reference','systeme reel','terme integral','observateur','observateur&terme integral')
